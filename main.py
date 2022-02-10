8a1fd675a230ced7ac08b1161f7f9dde462232cc
Gabriel Ford <gabe@fordltc.net>
2022-02-10 14:43:06 -0500

    setup environment variables for twilio

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import firebase_admin
 import time
 from firebase_admin import db
+from twilio.rest import Client
+import os
+
+account_sid = os.environ
+auth_token = os.environ
+print(account_sid["TWILIO_USER"])
 
 cred_obj = firebase_admin.credentials.Certificate(
     "./reactflightsearch-firebase-adminsdk-7vz6q-b6c4423253.json"
@@ -13,4 +19,10 @@ while True:
     time.sleep(1)
     to_notify = db.reference("/").get()["notify"]
     for notification in to_notify:
-        print(to_notify[notification])
+        # print(to_notify[notification])
+        airportFrom = to_notify[notification]["airportFrom"]
+        airportTo = to_notify[notification]["airportTo"]
+        dateFrom = to_notify[notification]["dateFrom"]
+        dateTo = to_notify[notification]["dateTo"]
+        maxPrice = to_notify[notification]["maxPrice"]
+        phone = to_notify[notification]["phone"]
